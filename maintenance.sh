b71e4eeabd58f1b4de5e60b6491797ad420faebe
vanderblugen <33649889+vanderblugen@users.noreply.github.com>
2021-05-28 09:04:57 -0500

    Create maintenance.sh

---
+++ maintenance.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+PATH=/bin:/usr/bin
+THISFILENAME="$(openssl rand -hex 8).txt"
+THISSTRING="\n*****************************************************************\n"
+
+echo "Maintenance for $HOSTNAME" >> /home/pi/$THISFILENAME
+echo -e $THISSTRING >> /home/pi/$THISFILENAME
+
+gpu=$(/opt/vc/bin/vcgencmd measure_temp | awk -F "[=\']" '{print $2}')
+cpu=$(</sys/class/thermal/thermal_zone0/temp)
+cpu=$(echo "$cpu / 100 * 0.1" | bc)
+cpuf=$(echo "(1.8 * $cpu) + 32" |bc)
+gpuf=$(echo "(1.8 * $gpu) + 32" |bc)
+
+echo "[Starting]" 2>&1 | ts >> /home/pi/$THISFILENAME
+echo "[Current GPU Temp] GPU => $gpu'C ($gpuf'F)" 2>&1 | ts >> /home/pi/$THISFILENAME
+echo "[Current CPU Temp] CPU => $cpu'C ($cpuf'F)" 2>&1 | ts >> /home/pi/$THISFILENAME
+echo -e $THISSTRING >> /home/pi/$THISFILENAME
+
+echo "[Running Update]" 2>&1 | ts >> /home/pi/$THISFILENAME
+apt-get update -y 2>&1 | ts >> /home/pi/$THISFILENAME
+echo -e $THISSTRING >> /home/pi/$THISFILENAME
+
+echo "[Running Upgrade]" 2>&1 | ts >> /home/pi/$THISFILENAME
+apt-get upgrade -y 2>&1 | ts >> /home/pi/$THISFILENAME
+echo -e $THISSTRING >> /home/pi/$THISFILENAME
+
+echo "[Running Dist-Upgrade]" 2>&1 | ts >> /home/pi/$THISFILENAME
+apt-get dist-upgrade -y 2>&1 | ts >> /home/pi/$THISFILENAME
+echo -e $THISSTRING >> /home/pi/$THISFILENAME
+
+echo "[Running Autoremove]" 2>&1 | ts >> /home/pi/$THISFILENAME
+apt-get autoremove -y 2>&1 | ts >> /home/pi/$THISFILENAME
+echo -e $THISSTRING >> /home/pi/$THISFILENAME
+
+echo "[Running Autoclean]" 2>&1 | ts >> /home/pi/$THISFILENAME
+apt-get autoclean -y 2>&1 | ts >> /home/pi/$THISFILENAME
+echo -e $THISSTRING >> /home/pi/$THISFILENAME
+
+echo "[Showing Free Space Information]" 2>&1 | ts >> /home/pi/$THISFILENAME
+sudo df 2>&1 | ts >> /home/pi/$THISFILENAME
+echo -e $THISSTRING >> /home/pi/$THISFILENAME
+
+echo "[Finishing and rebooting: sudo reboot now]" 2>&1 | ts >> /home/pi/$THISFILENAME
+echo -e $THISSTRING >> /home/pi/$THISFILENAME
+
+cat /home/pi/$THISFILENAME | gpg -ear "<email@1address1.com>" | mail -a "Subject: Monthly Maintenance" -a "X-Custom-Header: yes" "email@address2.com"
+
+srm -vz  /home/pi/$THISFILENAME
+unset THISFILENAME
+sudo reboot now
+
+exit 0

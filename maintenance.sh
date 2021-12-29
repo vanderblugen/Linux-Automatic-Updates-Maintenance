0547791ebdfc9982a1c4290ac26f49fbabe611ba
vanderblugen <33649889+vanderblugen@users.noreply.github.com>
2021-12-29 11:43:58 -0600

    Update maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -63,7 +63,7 @@ echo -e $THISSTRING >> ~/$THISFILENAME
 
 cat ~/$THISFILENAME | gpg -ear "<address1@mail.com>" | mail -a "Subject: Regular Maintenance" -a "X-Custom-Header: yes" "address2@mail.com"
 
-srm -vz  ~/$THISFILENAME
+shred -vz  ~/$THISFILENAME
 unset THISFILENAME
 sudo reboot now
 
